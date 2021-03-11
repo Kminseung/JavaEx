be5739126aefdbf30107f9b554a805619ce0948b
Kminseung <soccer6504@gmail.com>
2021-03-11 10:49:41 +0900

    Echo Server v2

--- JavaEx/src/com/javaex/network/echoserver/Server.java
+++ JavaEx/src/com/javaex/network/echoserver/Server.java
@@ -1,6 +1,10 @@
 package com.javaex.network.echoserver;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -24,8 +28,27 @@ public class Server {
 			Socket socket = serverSocket.accept();
 			// 클라이언트 정보 확인
 			InetSocketAddress socketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();	// 원격지 소켓의 주소 확인
-			System.out.println("SERVER : [클라이언트 연결 성공]");
-			System.out.println("CLIENT : " + socketAddress.getAddress() + socketAddress.getPort());
+			System.out.println("SERVER : [클라이언트 연결 성공] " + "CLIENT : " + socketAddress.getAddress() + socketAddress.getPort());
+			
+			//  메시지 수신
+			InputStream is = socket.getInputStream();
+			Reader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			
+			// 메시지 읽어오기
+			String message;
+			
+			while(true) {
+				message = br.readLine();
+				
+				if(message == null) {
+					System.out.println("SERVER : 접속 종료");
+					break;
+				}
+				
+				System.out.println("SERVER : [수신 메시지] : " + message);
+			}
+					
 			
 			// 후처리
 			System.out.println("SERVER : [서버 종료]");
